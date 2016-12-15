fcb96a93316f5d258684de6a2d3d3f230c97f4b9
Erwan Osouf <erwan.osouf@gmail.com>
2016-12-15 23:45:40 +0100

    Add more exercises for Lesson 4

---
+++ exercises/javascript/lesson4/exercise3.js
@@ -0,0 +1,14 @@
+var you = {
+  firstName : "",
+  lastName : "",
+  birthday : new Date(),
+  getAge : function() {
+
+  },
+  presentation : function() {
+
+  }
+}
+
+// Doit ecrire dans la console "Je m'appelle <firstName> <lastName> et j'ai <getAge()> ans"
+you.presentation();
